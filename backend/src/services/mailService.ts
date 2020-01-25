892ff3e722db343e5bf0501592452204c970e72d
Sampo Antila <sampo.antila@vincit.fi>
2020-01-25 18:41:47 +0200

    simplify backend and email sender ready

---
+++ backend/src/services/mailService.ts
@@ -0,0 +1,33 @@
+import sendgrid from '@sendgrid/mail';
+import { MailData } from "@sendgrid/helpers/classes/mail";
+
+class MailService {
+    async sendAuthenticationMail(email: string, token: string): Promise<void> {
+
+        const loginUrl = `http://localhost:4000/authenticate/validate/${token}`;
+
+        const msg: MailData = {
+            to: email,
+            from: process.env.SENDGRID_SENDER || '',
+            templateId: process.env.SENDGRID_TEMPLATE_ID,
+            dynamicTemplateData: {
+                loginUrl: loginUrl
+            }
+        };
+
+        var apiKey = process.env.SENDGRID_API_KEY;
+        if (apiKey)
+        {
+            sendgrid.setApiKey(apiKey);
+
+            const response = await sendgrid.send(msg);
+            console.log('message sent to ' + email);
+            console.log(response);
+        }
+        else {
+            console.warn('sendgrid api key missing, not sending email');
+        }
+    }
+}
+
+export default new MailService();
