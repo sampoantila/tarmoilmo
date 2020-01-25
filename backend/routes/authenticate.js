0dae63dd124b264102e62b16b361928954329a4f
Sampo Antila <sampo.antila@vincit.fi>
2020-01-25 12:48:40 +0200

    new layout for project

--- backend/routes/authenticate.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var authenticateController = require('../controllers/authenticateController');
-
-const router = express.Router();
-
-router.route('/:email')
-    .get(authenticateController.get);
-
-module.exports = router;
